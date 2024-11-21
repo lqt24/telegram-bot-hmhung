4ce97d4e8fb5efa2c6d01ae73d4289aead4b0a4e
kttl H <hung06032010@gmail.com>
2024-11-21 14:46:01 +0000

    support hosting

---
+++ delta.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+const TelegramBot = require('node-telegram-bot-api');
+const config = require('./config.json');
+const logger = require('./utils/log.js'); // logger by Niiozic
+
+const bot = new TelegramBot(config.token, { polling: config.polling });
+
+const commands = new Map();
+const cooldowns = new Map(); 
+
+const loadCommands = () => {
+    const commandFiles = fs.readdirSync('./modules').filter(file => file.endsWith('.js'));
+    let loadedCommandCount = 0;
+
+    commandFiles.forEach(file => {
+        try {
+            const command = require(`./modules/${file}`);
+            commands.set(command.config.name, command);
+            if (command.config.alias && Array.isArray(command.config.alias)) {
+                command.config.alias.forEach(alias => commands.set(alias, command));
+            }
+            loadedCommandCount++;
+            } catch (error) {
+            console.error(`Lỗi khi tải lệnh ${file}:`, error.message);
+        }
+    });
+
+    logger.loader(`Đã tải thành công ${loadedCommandCount}/${commandFiles.length} lệnh.`);
+};
+
+bot.on('polling_error', (error) => console.error('Lỗi khi polling:', error));
+bot.getMe().then((me) => {
+    logger.loader(`Đăng nhập thành công tại: @${me.username}`);
+});
+
+const isAdmin = (userId) => {
+    return config.admins.includes(userId); 
+};
+
+bot.on('message', (msg) => {
+    if (!msg.text || !msg.text.startsWith(config.prefix)) return;
+
+    const args = msg.text.slice(config.prefix.length).trim().split(" ");
+    const commandName = args.shift().toLowerCase();
+    const userId = msg.from.id;
+
+    if (commands.has(commandName)) {
+        const command = commands.get(commandName);
+
+        if (command.config.isAdmin && !isAdmin(userId)) {
+            bot.sendMessage(msg.chat.id, "Bạn không có quyền thực hiện lệnh này.");
+            return;
+        }
+
+        const now = Date.now();
+        const cooldownKey = `${commandName}-${userId}`;
+        const cooldownAmount = (command.config.cooldowns || 0) * 1000; 
+        if (cooldowns.has(cooldownKey)) {
+            const expirationTime = cooldowns.get(cooldownKey) + cooldownAmount;
+            if (now < expirationTime) {
+                const timeLeft = ((expirationTime - now) / 1000).toFixed(1);
+                bot.sendMessage(msg.chat.id, `Vui lòng đợi ${timeLeft} giây trước khi sử dụng lại lệnh /${commandName}.`);
+                return;
+            }
+        }
+
+        cooldowns.set(cooldownKey, now);
+        try {
+            command.run(bot, msg, args, commands); 
+        } catch (error) {
+            bot.sendMessage(msg.chat.id, "Đã xảy ra lỗi khi thực thi lệnh.");
+            console.error(`Lỗi khi thực hiện lệnh ${commandName}:`, error);
+        }
+    } else {
+        bot.sendMessage(msg.chat.id, "Lệnh không tồn tại!");
+    }
+});
+
+loadCommands();
--- index.js
+++ index.js
@@ -1,80 +1,38 @@
-const fs = require('fs');
-const TelegramBot = require('node-telegram-bot-api');
-const config = require('./config.json');
-const logger = require('./utils/log.js'); // logger by Niiozic
+const logger = require("./utils/log");
+const http = require("http");
+const { spawn, exec } = require('child_process');
 
-const bot = new TelegramBot(config.token, { polling: config.polling });
+const dashboard = http.createServer(function (_req, res) {
+    res.writeHead(200, "OKay", { "Content-Type": "text/html" });
+    res.write("Hello from Delta :>");
+    res.end();
+});
 
-const commands = new Map();
-const cooldowns = new Map(); 
+const port = process.env.PORT || 6788;
+dashboard.listen(port, () => {
+    logger(`Server is running on http://localhost:${port}`);
+});
 
-const loadCommands = () => {
-    const commandFiles = fs.readdirSync('./modules').filter(file => file.endsWith('.js'));
-    let loadedCommandCount = 0;
+function startBot(message) {
+    (message) ? logger(message, "『 starting 』 →") : "";
 
-    commandFiles.forEach(file => {
-        try {
-            const command = require(`./modules/${file}`);
-            commands.set(command.config.name, command);
-            if (command.config.alias && Array.isArray(command.config.alias)) {
-                command.config.alias.forEach(alias => commands.set(alias, command));
-            }
-            loadedCommandCount++;
-            logger.loader(`Đã tải lệnh: ${command.config.name}`);
-        } catch (error) {
-            console.error(`Lỗi khi tải lệnh ${file}:`, error.message);
-        }
+    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "delta.js"], {
+        cwd: __dirname,
+        stdio: "inherit",
+        shell: true
     });
 
-    logger.loader(`Đã tải thành công ${loadedCommandCount}/${commandFiles.length} lệnh.`);
-};
-
-bot.on('polling_error', (error) => console.error('Lỗi khi polling:', error));
-bot.getMe().then((me) => {
-    logger.loader(`Đăng nhập thành công tại: @${me.username}`);
-});
-
-const isAdmin = (userId) => {
-    return config.admins.includes(userId); 
-};
-
-bot.on('message', (msg) => {
-    if (!msg.text || !msg.text.startsWith(config.prefix)) return;
-
-    const args = msg.text.slice(config.prefix.length).trim().split(" ");
-    const commandName = args.shift().toLowerCase();
-    const userId = msg.from.id;
-
-    if (commands.has(commandName)) {
-        const command = commands.get(commandName);
-
-        if (command.config.isAdmin && !isAdmin(userId)) {
-            bot.sendMessage(msg.chat.id, "Bạn không có quyền thực hiện lệnh này.");
+    child.on("close", (codeExit) => {
+        if (codeExit != 0 || global.countRestart && global.countRestart < 5) {
+            startBot("Restarting...");
+            global.countRestart += 1;
             return;
-        }
+        } else return;
+    });
 
-        const now = Date.now();
-        const cooldownKey = `${commandName}-${userId}`;
-        const cooldownAmount = (command.config.cooldowns || 0) * 1000; 
-        if (cooldowns.has(cooldownKey)) {
-            const expirationTime = cooldowns.get(cooldownKey) + cooldownAmount;
-            if (now < expirationTime) {
-                const timeLeft = ((expirationTime - now) / 1000).toFixed(1);
-                bot.sendMessage(msg.chat.id, `Vui lòng đợi ${timeLeft} giây trước khi sử dụng lại lệnh /${commandName}.`);
-                return;
-            }
-        }
+    child.on("error", function (error) {
+        logger("An error occurred: " + JSON.stringify(error), "『 starting 』 →");
+    });
+}
 
-        cooldowns.set(cooldownKey, now);
-        try {
-            command.run(bot, msg, args, commands); 
-        } catch (error) {
-            bot.sendMessage(msg.chat.id, "Đã xảy ra lỗi khi thực thi lệnh.");
-            console.error(`Lỗi khi thực hiện lệnh ${commandName}:`, error);
-        }
-    } else {
-        bot.sendMessage(msg.chat.id, "Lệnh không tồn tại!");
-    }
-});
-
-loadCommands();
+startBot();
