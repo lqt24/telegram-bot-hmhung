d74fad4715ad2e33b2b5418adfb32efd6c181938
kttl H <hung06032010@gmail.com>
2024-11-21 14:24:48 +0000

    Tạm thế này đã

---
+++ modules/tts.js
@@ -0,0 +1,11 @@
+module.exports.config = {
+    name: "tts",
+    isAdmin: false,
+    cooldowns: 5,
+    credits: "hmhung",
+    usage: "tts [text]"
+}
+
+module.exports.run = async(bot, msg) => {
+    //Để tạm đây
+}
