e6dc42297730dfad4d260ec21ac006582659026d
kttl H <hung06032010@gmail.com>
2024-11-22 10:07:01 +0000

    add tts

--- modules/help.js
+++ modules/help.js
@@ -1,37 +1,38 @@
 module.exports.config = {
-  name: "help",
-  description: "Hiển thị danh sách các lệnh hiện có hoặc thông tin chi tiết về một lệnh.",
-  usage: "/help [tên lệnh]",
-  isAdmin: false,
-  credits: "hmhung",
-  cooldowns: 20
+    name: "help",
+    description: "Hiển thị danh sách các lệnh hiện có hoặc thông tin chi tiết về một lệnh.",
+    usage: "/help [tên lệnh]",
+    isAdmin: false,
+    credits: "hmhung",
+    cooldowns: 20
 };
 
 module.exports.run = (bot, msg, args, commands) => {
-  const chatId = msg.chat.id;
-  const commandName = args[0];
+    const chatId = msg.chat.id;
+    const commandName = args[0];
 
-  if (commandName) {
-      const command = commands.get(commandName.toLowerCase());
-      if (command) {
-          const { name, description, usage, credits, cooldowns, alias} = command.config;
-          const helpMessage = `
-          Thông tin về lệnh: /${name}
-          Mô tả: ${description}
-          Cách sử dụng: ${usage}
-          Credits: ${credits || 'Không có'}
-          Cooldowns: ${cooldowns || 'Không có'}
-	      Tên khác: ${alias || 'Không có'}
-          `;
-          bot.sendMessage(chatId, helpMessage.trim());
-      } else {
-          bot.sendMessage(chatId, "Lệnh không tồn tại.");
-      }
-  } else {
-      let helpMessage = "Các lệnh có thể sử dụng:\n";
-      commands.forEach((command) => {
-          helpMessage += `/${command.config.name} - ${command.config.description}\n`;
-      });
-      bot.sendMessage(chatId, helpMessage.trim());
-  }
+    if (commandName) {
+        const command = commands.get(commandName.toLowerCase());
+        if (command) {
+            const { name, description, usage, credits = 'Không có', cooldowns = 'Không có', alias = 'Không có' } = command.config;
+            const helpMessage = `
+            Thông tin về lệnh: "${name}"
+            Mô tả: ${description}
+            Cách sử dụng: ${usage}
+            Credits: ${credits}
+            Cooldowns: ${cooldowns}
+            Tên khác: ${alias}
+            `;
+            bot.sendMessage(chatId, helpMessage.trim());
+        } else {
+            bot.sendMessage(chatId, "Lệnh không tồn tại.");
+        }
+    } else {
+        const totalCommands = commands.size;  // Tổng số lệnh
+        const helpMessage = `Tổng số lệnh hiện có: ${totalCommands}\nCác lệnh có thể sử dụng:\n\n${Array.from(commands.values())
+                .map((command) => `/${command.config.name} - ${command.config.description}`)
+                .join("\n")}
+        `;
+        bot.sendMessage(chatId, helpMessage.trim());
+    }
 };
--- modules/tts.js
+++ modules/tts.js
@@ -1,12 +1,57 @@
+const { createReadStream, unlinkSync, writeFileSync } = require("fs-extra");
+const { resolve } = require("path");
+const axios = require('axios');
+
 module.exports.config = {
     name: "tts",
-    isAdmin: false,
-    cooldowns: 5,
-    description: "Text-To-Speech",
-    credits: "hmhung",
-    usage: "tts [text]"
-}
+    version: "1.0.1",
+    description: "Khiến bot trả về file âm thanh của chị Google thông qua văn bản.",
+    usage: "/tts [ru/en/ko/ja] [Text]",
+    cooldown: 5,
+    credits: "Mirai Team, convert by Hà Mạnh Hùng"
+};
 
-module.exports.run = async(bot, msg) => {
-    //Để tạm đây
-}
+module.exports.run = async (bot, msg, args) => {
+    try {
+        const chatId = msg.chat.id;
+
+        const path = resolve(__dirname, 'cache', `${Date.now()}.mp3`);
+
+        const content = args.join(" ");
+        const languageToSay = (["ru", "en", "ko", "ja"].some(item => content.startsWith(item)))
+            ? content.slice(0, content.indexOf(" "))
+            : "vi";
+        const text = (languageToSay !== "vi") ? content.slice(3) : content;
+
+        const maxLength = 200;
+        let audioData = Buffer.alloc(0);
+
+        const chunks = [];
+        let remainingText = text;
+        while (remainingText.length > 0) {
+            const chunk = remainingText.slice(0, maxLength);
+            chunks.push(chunk);
+            remainingText = remainingText.slice(maxLength);
+        }
+
+        for (const chunk of chunks) {
+            const url = `https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(chunk)}&tl=${languageToSay}&client=tw-ob`;
+            const response = await axios.get(url, { responseType: 'arraybuffer' });
+            audioData = Buffer.concat([audioData, response.data]);
+        }
+
+        writeFileSync(path, audioData);
+        
+        const fileOptions = {
+            filename: `${chatId}_${msg.from.id}.mp3`,
+            contentType: "audio/mpeg"
+        };
+        const stream = await createReadStream(path)
+        await bot.sendAudio(msg.chat.id, stream, {}, fileOptions);
+
+        unlinkSync(path);
+    } catch (error) {
+        console.error(error);
+        await bot.sendMessage(msg.chat.id, "Đã xảy ra lỗi khi tạo file âm thanh!");
+    }
+};
